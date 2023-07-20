314cb999f0818f6f9b98cd6aa465cb754c8046ad
kamikaros <aguliaeva@unibz.it>
2023-07-20 12:12:18 +0200

    added gui files

---
+++ src/main/java/com/example/programmingproject/gui/HelloController.java
@@ -0,0 +1,28 @@
+// 
+// Decompiled by Procyon v0.5.36
+// 
+
+package com.example.demo;
+
+import java.io.IOException;
+import javafx.scene.Scene;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.stage.Stage;
+import javafx.scene.input.MouseEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class HelloController
+{
+    @FXML
+    private Button button1;
+    
+    @FXML
+    void switchToMenu(final MouseEvent event) throws IOException {
+        final Stage stage = (Stage)this.button1.getScene().getWindow();
+        final Parent root = (Parent)FXMLLoader.load(HelloApplication.class.getResource("menu.fxml"));
+        stage.setScene(new Scene(root));
+        stage.setTitle("The Fancy Sudoku!");
+    }
+}
