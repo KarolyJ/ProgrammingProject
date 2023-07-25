43307bcfd5fd5b202c31f7ebd4bb130bff1b1fc5
Liliia Loboda <lilyaloboda2002@gmail.com>
2023-07-25 17:24:20 +0200

    Feat(Test): testing the amount of zeros in our sudoku with different levelOfTheGame

--- src/main/java/com/example/programmingproject/test/SudokuTest.java
+++ src/main/java/com/example/programmingproject/test/SudokuTest.java
@@ -1,5 +1,7 @@
 package com.example.programmingproject.test;
 
+import com.example.programmingproject.logic.Grid;
+import com.example.programmingproject.logic.Sudoku;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -25,6 +27,42 @@ public class SudokuTest {
         };
 
         int expectedZeros = 5; // The expected number of zeros in the array
+        int actualZeros = SudokuTest.countZeros(array);
+
+        // Assert that the actual number of zeros matches the expected number
+        assertEquals(expectedZeros, actualZeros);
+    }
+    @Test
+    public void testRemoveKDigitsWith20() {
+        Grid grid = new Grid();
+        grid.setLevelOfTheGame(20);
+        grid.hideSudoku();
+        int[][] array = grid.getBase();
+        int expectedZeros = grid.getLevelOfTheGame(); // The expected number of zeros in the array
+        int actualZeros = SudokuTest.countZeros(array);
+
+        // Assert that the actual number of zeros matches the expected number
+        assertEquals(expectedZeros, actualZeros);
+    }
+    @Test
+    public void testRemoveKDigitsWith30() {
+        Grid grid = new Grid();
+        grid.setLevelOfTheGame(30);
+        grid.hideSudoku();
+        int[][] array = grid.getBase();
+        int expectedZeros = grid.getLevelOfTheGame(); // The expected number of zeros in the array
+        int actualZeros = SudokuTest.countZeros(array);
+
+        // Assert that the actual number of zeros matches the expected number
+        assertEquals(expectedZeros, actualZeros);
+    }
+    @Test
+    public void testRemoveKDigitsWith40() {
+        Grid grid = new Grid();
+        grid.setLevelOfTheGame(40);
+        grid.hideSudoku();
+        int[][] array = grid.getBase();
+        int expectedZeros = grid.getLevelOfTheGame(); // The expected number of zeros in the array
         int actualZeros = SudokuTest.countZeros(array);
 
         // Assert that the actual number of zeros matches the expected number
