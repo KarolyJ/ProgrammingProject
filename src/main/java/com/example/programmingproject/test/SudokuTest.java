939a085144b5d743b9c56f5d328f786f25f53fd5
Liliia Loboda <lilyaloboda2002@gmail.com>
2023-07-25 17:31:45 +0200

    Fix(SudokuTest): deleting unusable import

--- src/main/java/com/example/programmingproject/test/SudokuTest.java
+++ src/main/java/com/example/programmingproject/test/SudokuTest.java
@@ -1,7 +1,6 @@
 package com.example.programmingproject.test;
 
 import com.example.programmingproject.logic.Grid;
-import com.example.programmingproject.logic.Sudoku;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
