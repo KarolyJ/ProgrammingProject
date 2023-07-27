d8f96999d3a615b0ae4d49bfa42b743035931035
Liliia Loboda <lilyaloboda2002@gmail.com>
2023-07-27 18:03:34 +0200

    Feat(Grid): using SudokuGenerator as a generator for Grid

--- src/main/java/com/example/programmingproject/logic/Grid.java
+++ src/main/java/com/example/programmingproject/logic/Grid.java
@@ -4,23 +4,27 @@ package com.example.programmingproject.logic;
 import java.util.Arrays;
 
 public class Grid {
-    private final int[][] base = new int[][]{
-            {2, 3, 9, 4, 1, 5, 7, 6, 8},
-            {7, 8, 4, 2, 3, 6, 5, 1, 9},
-            {1, 6, 5, 9, 8, 7, 2, 3, 4},
-            {3, 1, 7, 6, 9, 4, 8, 2, 5},
-            {4, 5, 8, 1, 2, 3, 6, 9, 7},
-            {9, 2, 6, 7, 5, 8, 3, 4, 1},
-            {8, 4, 3, 5, 6, 9, 1, 7, 2},
-            {6, 7, 1, 8, 4, 2, 9, 5, 3},
-            {5, 9, 2, 3, 7, 1, 4, 8, 6}};
+//    private final int[][] base = new int[][]{
+//            {2, 3, 9, 4, 1, 5, 7, 6, 8},
+//            {7, 8, 4, 2, 3, 6, 5, 1, 9},
+//            {1, 6, 5, 9, 8, 7, 2, 3, 4},
+//            {3, 1, 7, 6, 9, 4, 8, 2, 5},
+//            {4, 5, 8, 1, 2, 3, 6, 9, 7},
+//            {9, 2, 6, 7, 5, 8, 3, 4, 1},
+//            {8, 4, 3, 5, 6, 9, 1, 7, 2},
+//            {6, 7, 1, 8, 4, 2, 9, 5, 3},
+//            {5, 9, 2, 3, 7, 1, 4, 8, 6}};
+
+    private final int[][] base;
+    SudokuGenerator sudoku = new SudokuGenerator(9);
 
     //TODO merge it with the class Sudoku
 
     private int[][] readySudoku;
 
     public Grid() {
-        //setReadySudoku();
+        sudoku.fillValues();
+        base = sudoku.mat;
         setLevelOfTheGame(MIN_LEVEL); //TODO change for later
     }
 
