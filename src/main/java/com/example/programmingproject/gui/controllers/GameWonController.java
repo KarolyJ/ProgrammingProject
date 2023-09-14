8aced614d6c5321148c1dc7bb4171fff1de87fb6
Liliia Loboda <lilyaloboda2002@gmail.com>
2023-09-14 14:37:13 +0200

    Feat(Game): cleaning the code

--- src/main/java/com/example/programmingproject/gui/controllers/GameWonController.java
+++ src/main/java/com/example/programmingproject/gui/controllers/GameWonController.java
@@ -18,9 +18,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 public class GameWonController {
 
